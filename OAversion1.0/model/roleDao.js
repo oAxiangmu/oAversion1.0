0596b35e713f68e763c6b7c16691ef4179227fc9
Administrator <782096085@qq.com>
2017-09-13 13:14:20 +0800

    20170913角色已完成唐翠

--- OAversion1.0/model/roleDao.js
+++ OAversion1.0/model/roleDao.js
@@ -1,5 +1,11 @@
 var connection=require("./connection.js");
 var role={
+    //角色查找
+       list:function (data,callback) {
+           connection.query("select * from oa_role where is_del=0  limit  ?,? ", data, function (err, rows) {
+               callback(err, rows);
+           });
+       },
 	//数据添加
 		add:function(data,callback){
 			connection.query("insert into oa_role (name,creat_time) values(?,now())",data,function(err,result){
@@ -11,18 +17,18 @@ var role={
 	isExesit:function (params,callback) {
 		    var sql="";
 		    if(params.name){
-                 sql="select  id from oa_role where name="+params.name;
+                 sql="select  id from oa_role where name="+params.name+"where is_del=0";
             }else{
-                sql="select  id from oa_role where id="+params.id;
+                sql="select  id from oa_role where id="+params.id+"where is_del=0";
             }
-
+     console.log(params);
         connection.query(sql,params,function(err,result){
             callback(err,result);
         });
     },
     //角色删除
     delet:function (data,callback) {
-        connection.query("delete  from  oa_role where id=?",data,function(err,result){
+        connection.query("update oa_role  set is_del=1 where id=?",data,function(err,result){
             callback(err,result);
         });
     },
--- OAversion1.0/routes/role.js
+++ OAversion1.0/routes/role.js
@@ -14,14 +14,14 @@ router.post("/add", function (req, res, next) {
 
 
     roleDao.isExesit(params, function (error, rows) {
-        if (rows) {
+        if (!rows) {
             data.result = 0;
             data.message = {
                 res:"角色创建失败'",
                detail:"角色已经存在",
         };
             data.data=null;
-          res.json(data);
+
         }
     });
     roleDao.add(name, function (error, result) {
@@ -30,9 +30,10 @@ router.post("/add", function (req, res, next) {
             data.result = 0;
             data.message = {result: "数据插入失败", detail: error};
             data.data = null;
-            res.json(data);
 
-        } else {
+
+
+        }
             data.result = 1;
             data.message = {
                 result:"数据插入成功",
@@ -40,14 +41,29 @@ router.post("/add", function (req, res, next) {
             };
             data.data = null;
             res.json(data);
-        }
+
+
 
     });
 });
 //角色删除
 router.get("/delet/:id", function (req, res, next) {
+    var param={};
     var id=req.params.id;
     var data={};
+    param.id=id;
+
+    roleDao.isExesit(param, function (error, rows) {
+        if (rows) {
+            data.result = 0;
+            data.message = {
+                res:"角色删除失败'",
+                detail:"角色不存在",
+            };
+            data.data=null;
+
+        }
+    });
     roleDao.delet(id,function (error,result) {
           if(error){
             data.result=0;
@@ -55,7 +71,7 @@ router.get("/delet/:id", function (req, res, next) {
                 result:"角色删除失败",
                 detail:error,
             }
-            res.json(data);
+
           }
           data.result=1;
           data.message={
@@ -64,6 +80,36 @@ router.get("/delet/:id", function (req, res, next) {
           };
           data.data=null;
           res.json(data);
+
     })
+});
+//查找角色
+router.get("/list/:page",function (req,res,next) {
+
+    var data={};
+    var params=[];
+    var pageSize=10;
+   var page= req.params.page||1;
+   var start=(page-1)*pageSize;
+   params=[start,pageSize];
+   roleDao.list(params,function (error,result) {
+       if(error){
+             data.result=0;
+             data.message= {
+                 res: "角色查询失败",
+                 detail: error,
+       };
+             data.data=null;
+       }
+
+       data.result=1;
+       data.message={
+           res:"角色查询成功",
+           detail:"",
+       }
+       data.data=result;
+       res.json(data);
+
+   })
 });
 module.exports = router;
